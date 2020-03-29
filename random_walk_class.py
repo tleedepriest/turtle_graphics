fca92f640eef2cb95c3df33942a775248c9fe10a
Tdepries <tdepries@ida.org>
2020-03-29 11:56:50 -0400

    diveded the RandomWalk class into two attributes move and turn in order to comply with pylint standards

--- random_walk_class.py
+++ random_walk_class.py
@@ -1,72 +1,131 @@
 """
 Class to create a randomwalk object
 """
+import random
 
-class RandomWalk:
+class RandomWalk(object):
     """
     A 2D randomwalk object. Variables included in the random
     walk are in the __init__ function.
     """
-    def __init__(self, for_len_r, back_len_r, penup_prob, steps):
+    def __init__(self, turtle, steps):
 
         """
         Parameters 
         ----------
+
+        
+        Returns
+        ----------
+        None
+        """
+        self.turtle = turtle
+        self.steps = steps
+    
+    def draw_random_walk(self):
+        """
+        Parameters
+        -------------
+        self
+
+        Returns
+        -------------
+        None 
+
+        This is the main function which executes the two
+        functions below
+        """
+        for _ in range(self.steps):
+            self.move()
+            self.turn()
+
+    def move(self, for_len=(25, 50), back_len=(25, 50), penup_prob=0):
+        """
+        Parameters
+        ------------
+        for_len_r: tuple containing ints --> (25, 50)
+            the turtle will randomly choose a value
+            between 25 and 50 and move forward that amount
+
+        OR
+
         for_len_r: int
-            integer lower limit on forward movement
+            turtle always moves forward this length
 
-        for_len_r2: int
-            integer upper limit on forward movementf
+        ####################################################
 
-        the turtle will randomly choose an integer
-        between for_len_r1 and for_len_r2 and move
-        forward that amount
+        back_len_r: tuple containing ints --> (25, 50)
+            the turtle will randomly choose an integer
+            between 25 and 50 and move backward that amount
 
-        back_len_r1: int
-            integer lower limit on backward movement
+        OR
 
-        back_len_r2: int
-            integer upper limit on forward movement
+        back_len_r: int
+            the turtle will always move backward this amount
 
-        the turtle will randomly choose an integer
-        between back_len_r1 and back_len_r2 and move
-        backward that amount
+        ####################################################
 
         penup_prob: int
-            number between 0-1 which represents the percentage of 
-            time that the turtle lifts the pen up as it performs
-            the 2d random walk. set this to 0 if you want no 
-            gaps in the movement
-        """
+            an number between 0 and 1 (0 inclusive 1 exclusive)
+            0 means the turtle will never be lifted while the turtle
+            draws .99 means the turtle will be lifted 99% of the time
+            while the turtle draws. Higher number, less visible lines.
 
-        self.for_len_r1, self.for_len_r2 = for_len_r
-        self.back_len_r1, self.back_len_r2 = back_len_r
-        self.penup_prob = penup_prob
-        self.steps = steps
+            no more than digits after decimal
+        
+        Returns
+        --------------
+        None
+        
+        this function moves the turtle forward half of the time and
+        backward half of the time while drawing at least
+        """
+        def check_type(tuple_or_int):
+            """
+            Parameters
+            -----------
+            tuple_or_int: tuple or int
+                if tuple generate random number in tuple range
+                if int do nothin
+            
+            Returns
+            ----------
+            length: int
+                the lenth the turtle will move
+            """
 
-    def draw(self):
-        """
-        This function takes the variables from 
-        __init__ and uses them to generate a
-        2D random walk
-        """
-        import turtle
-        import random
-        t = turtle.Turtle()
-        for x in range(self.steps):
-            switch = random.randint(0,1) 
-            penup_switch = (random.randrange(0,10))/10
-            if switch:
-                if penup_switch < self.penup_prob:
-                    t.penup()
-                t.forward(random.randrange(self.for_len_r1, self.for_len_r2))
+            if isinstance(tuple_or_int, tuple):
+                length = random.randrange(*tuple_or_int)
+            elif isinstance(tuple_or_int, int):
+                length = tuple_or_int
             else:
-                if penup_switch < self.penup_prob:
-                    t.penup()
-                t.backward(random.randrange(self.back_len_r1, self.back_len_r2))
-            t.pendown()
-            switch = random.randint(0,1)
-            if switch:
-                t.right(90)
-            else:
-                t.left(90)
+                raise ValueError("for_len must be of type tuple or int")
+
+            return length
+
+        switch = random.randint(0, 1)
+        penup_switch = random.uniform(0, 1)
+
+        if switch:
+            if penup_switch < penup_prob:
+                self.turtle.penup()
+            self.turtle.forward(check_type(for_len))
+            
+        else:
+            if penup_switch < penup_prob:
+                self.turtle.penup()
+            self.turtle.backward(check_type(back_len))
+
+        self.turtle.pendown()
+
+    def turn(self, right_angle=90, left_angle=90):
+        """
+        please the pylint gods
+        """
+        switch = random.randint(0, 1)
+
+        if switch:
+            self.turtle.right(right_angle)
+        else:
+            self.turtle.left(left_angle)
+
